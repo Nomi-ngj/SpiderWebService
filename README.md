961e3cfa0fed373eb5a2851c36ce74fc99a78df5
Nouman Gul <Noumanguljunejo@gmail.com>
2023-03-28 12:09:22 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpiderWebService
+
+A description of this package.
---
+++ Tests/SpiderWebServiceTests/SpiderWebServiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SpiderWebService
+
+final class SpiderWebServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SpiderWebService().text, "Hello, World!")
+    }
+}
